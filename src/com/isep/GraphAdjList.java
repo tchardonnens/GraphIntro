d54fb367d83bac5098380aec8c074128c93e7205
Thomas Chardo <tchardonnens@juniorisep.com>
2023-04-18 10:49:44 +0200

    GraphAdjList done

--- src/com/isep/GraphAdjList.java
+++ src/com/isep/GraphAdjList.java
@@ -1,16 +1,14 @@
 package com.isep;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class GraphAdjList {
-    private int N;
-    private int M;
-    private List<List<Integer>> adj;
+    private int N; // number of nodes
+    private int M; // number of edges
+    private List<List<Integer>> adj; // adjacency list representation
 
+    // Initialize an empty graph of order N (input parameter) and 0 edges.
     public GraphAdjList(int N) {
         this.N = N;
         this.M = 0;
@@ -20,27 +18,25 @@ public class GraphAdjList {
         }
     }
 
-    public GraphAdjList(String filename) {
-        try {
-            Scanner scanner = new Scanner(new File(filename));
-            N = scanner.nextInt();
-            M = scanner.nextInt();
-            adj = new ArrayList<>();
-            for (int i = 0; i < N; i++) {
-                adj.add(new ArrayList<>());
-            }
-
-            while (scanner.hasNext()) {
-                int u = scanner.nextInt() - 1;
-                int v = scanner.nextInt() - 1;
-                addEdge(u, v);
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    // Initializes a graph from a specified input stream.
+    public GraphAdjList(InputStream inputStream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(inputStream);
+        this.N = 4; // number of nodes is 4
+        this.M = 6; // number of edges is 6
+        adj = new ArrayList<>();
+        for (int i = 0; i <= N; i++) { // <= N to accommodate for 1-based indexing of nodes
+            adj.add(new ArrayList<>());
         }
+        while (scanner.hasNext()) { // read edges until the end of the file
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            addEdge(u, v);
+        }
+        scanner.close();
     }
 
+
+    // Return the total order and the size of the graph.
     public int getOrder() {
         return N;
     }
@@ -49,28 +45,117 @@ public class GraphAdjList {
         return M;
     }
 
+    // Add an edge between two existing nodes to the graph.
     public void addEdge(int u, int v) {
         adj.get(u).add(v);
         adj.get(v).add(u);
-        M++;
     }
 
-    public List<Integer> neighbors(int v) {
-        return adj.get(v);
+    // Create a function Neighbors(int v) that takes as an input a given vertex and prints all the neighbors of that vertex.
+    public void neighbors(int v) {
+        System.out.println("Neighbors of vertex " + v + ": " + adj.get(v));
     }
 
-    public void printGraph() {
-        for (int i = 0; i < N; i++) {
-            System.out.print(i + ": ");
-            for (int neighbor : adj.get(i)) {
-                System.out.print(neighbor + " ");
-            }
-            System.out.println();
+    // Add a function that prints the Adjacency list representation of the graph.
+    public void printAdjList() {
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.println(i + " -> " + adj.get(i));
         }
     }
 
+    // Create a function called Degree(int v) that returns for a given vertex v, its degree.
     public int degree(int v) {
         return adj.get(v).size();
     }
 
+    // 1. What is the average, minimal and maximal degree of the graph?
+    public double averageDegree() {
+        int totalDegree = 0;
+        for (int i = 0; i < N; i++) {
+            totalDegree += degree(i);
+        }
+        return (double) totalDegree / N;
+    }
+
+    public int minDegree() {
+        int minDegree = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            minDegree = Math.min(minDegree, degree(i));
+        }
+        return minDegree;
+    }
+
+    public int maxDegree() {
+        int maxDegree = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            maxDegree = Math.max(maxDegree, degree(i));
+        }
+        return maxDegree;
+    }
+
+    // 1. What is the edge-density?
+    public double edgeDensity() {
+        return (double) (2 * M) / (N * (N - 1));
+    }
+
+    // 2. Are there any isolated nodes? If yes, which ones?
+    public List<Integer> isolatedNodes() {
+        List<Integer> isolated = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            if (degree(i) == 0) {
+                isolated.add(i);
+            }
+        }
+        return isolated;
+    }
+
+    // 3. Are there any loops?
+    public boolean hasLoops() {
+        for (int i = 0; i < N; i++) {
+            if (adj.get(i).contains(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Read graph data from keyboard input
+    public static GraphAdjList readGraphFromInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number of vertices? ");
+        int n = scanner.nextInt();
+        System.out.print("Number of edges? ");
+        int m = scanner.nextInt();
+        GraphAdjList graph = new GraphAdjList(n);
+        for (int i = 0; i < m; i++) {
+            System.out.print("Edge " + (i + 1) + "? ");
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            graph.addEdge(u, v);
+        }
+        scanner.close();
+        return graph;
+    }
+
+    // Main function to test the above methods
+    public static void main(String[] args) throws FileNotFoundException {
+        // Read graph from file
+        FileInputStream fis = new FileInputStream("src/com/isep/graph.txt");
+        GraphAdjList graph = new GraphAdjList(fis);
+        graph.printAdjList();
+
+        // Test functions
+        graph.neighbors(1);
+        System.out.println("Degree of vertex 1: " + graph.degree(1));
+        System.out.println("Average degree: " + graph.averageDegree());
+        System.out.println("Min degree: " + graph.minDegree());
+        System.out.println("Max degree: " + graph.maxDegree());
+        System.out.println("Edge density: " + graph.edgeDensity());
+        System.out.println("Isolated nodes: " + graph.isolatedNodes());
+        System.out.println("Has loops: " + graph.hasLoops());
+
+        // Read graph from keyboard input
+        GraphAdjList inputGraph = readGraphFromInput();
+        inputGraph.printAdjList();
+    }
 }
--- src/com/isep/Main.java
+++ src/com/isep/Main.java
@@ -2,23 +2,7 @@ package com.isep;
 
 public class Main {
     public static void main(String[] args) {
-        // Test GraphAdjList
-        String fileName = "src/com/isep/graph.txt";
-        GraphAdjList graphAdjList = new GraphAdjList(fileName);
 
-        System.out.println("GraphAdjList:");
-        graphAdjList.printGraph();
 
-        // Test GraphAdjMatrix
-        GraphAdjMatrix graphAdjMatrix = new GraphAdjMatrix(fileName);
-        int[][] distances = graphAdjMatrix.calculateDistances();
-
-        System.out.println("\nGraphAdjMatrix distances:");
-        for (int i = 0; i < distances.length; i++) {
-            for (int j = 0; j < distances[i].length; j++) {
-                System.out.print(distances[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 }
